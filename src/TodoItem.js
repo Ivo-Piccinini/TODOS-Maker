71c95995e122bdaecfa4dd40b46f2d6c063b23f3
IvoPiccinini <ivopiccinini02@gmail.com>
2022-10-20 15:58:31 -0300

    manejo de eventos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,11 +2,31 @@ import React from "react";
 import './TodoItem.css';
 
 function TodoItem(props) {
+    const onComplete = () => {
+        alert('ya completaste el todo ' +'"'+ props.text +'"')
+    };
+
+    const onDelete = () => {
+        alert('borraste el todo ' +'"'+ props.text +'"')
+    };
+
     return (
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}><span className={`Icon_img ${props.completed && 'Icon_img--active'}`}></span></span>
+            <span
+                className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+                onClick={onComplete}
+                >
+
+            <span className={`Icon_img ${props.completed && 'Icon_img--active'}`}></span>
+            </span>
+
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
-            <span className="Icon Icon-delete">x</span>
+            <span 
+                className="Icon Icon-delete"
+                onClick={onDelete}
+                >
+                    x
+            </span>
         </li>
     );
 }
